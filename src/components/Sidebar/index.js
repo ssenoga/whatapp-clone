1443d47080ecad3bf6b69698b38615ea0660865e
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-23 11:40:00 +0300

    Added the react-router-dom

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+
+import "./sidebar.css";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header"></div>
-      <div className="sidebar__search"></div>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="side__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search here ..." />
+        </div>
+      </div>
       <div className="sidebar__chats"></div>
-      <h1>Sidebar </h1>
     </div>
   );
 }
